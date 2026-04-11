993cbff5178d64313f1735b0f94a44fd543e4847
muyaaho <skanthsu715@gmail.com>
2026-04-11 10:52:46 +0900

    feat: order-service 구현중 260411

--- src/main/kotlin/com/ecommerce/order/service/OrderService.kt
+++ src/main/kotlin/com/ecommerce/order/service/OrderService.kt
@@ -1,10 +1,23 @@
 package com.ecommerce.order.service
 
-import org.springframework.stereotype.Repository
+import com.ecommerce.order.client.ProductServiceClient
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class OrderService(
-    private val orderRepository: OrderRepo
+    private val orderRepository: OrderRepository,
+    private val productServiceClient: ProductServiceClient // 1. Feign Client 주입
 ) {
+    @Transactional
+    fun createOrder(memberId: Long, request: CreateOrderRequest) {
+        // 2. 마치 로컬 메서드를 호출하듯 간결하게 API 호출
+        val product = productServiceClient.getProduct(request.productId)
+
+        // 3. 재고 확인 로직
+        if (product.stockQuantity < request.quantity) {
+            throw IllegalArgumentException("재고가 부족합니다.")
+        }
+        // ... (주문 생성 로직) ...
+    }
 }
